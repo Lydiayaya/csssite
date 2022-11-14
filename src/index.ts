4e8cb8c236a9573b949536170b311f59a3aa090b
lydiayaya <2103207398@qq.com>
2022-11-14 16:59:43 +0800

    style: prepare for sass

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const calc = (a: number, b: number): number => {
-  return a - b
-}
-console.log(calc(1024, 28))
-export default calc
+  return a - b;
+};
+console.log(calc(1024, 28));
+export default calc;
